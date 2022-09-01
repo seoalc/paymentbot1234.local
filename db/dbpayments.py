b9f8c8a386a169f308268efae8b91630501d7c08
unknown <rrryyy07@gmail.com>
2022-09-01 13:59:18 +0600

    finished the payment module

--- db/dbpayments.py
+++ db/dbpayments.py
@@ -1,7 +1,7 @@
 import pymysql.cursors
 
 ############# проверка наличия исполнителя в базе по tg id ################
-def checkUserByTgId (tg_id):
+def checkPaymentById (id):
     # Подключиться к базе данных.
     connection = pymysql.connect(host='127.0.0.1',
                                  user='root',
@@ -9,21 +9,21 @@ def checkUserByTgId (tg_id):
                                  db='paymentbot',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
-    print ("connect successful checkUserByTgId!")
+    print ("connect successful checkPaymentById!")
     try:
         with connection.cursor() as cursor:
             # SQL
-            sql = "SELECT `id` FROM `users` WHERE `tg_id` = %s"
+            sql = "SELECT `id` FROM `payments` WHERE `bill_id` = %s"
             # Выполнить команду запроса (Execute Query).
             # есть пользователь с этим ником в базе
-            res = cursor.execute(sql, (tg_id))
+            res = cursor.execute(sql, (id))
             return res
     finally:
         # Закрыть соединение (Close connection).
         connection.close()
 
-############# получение имеющейся суммы у пользователя по tg id ################
-def getUsersCheckByTgId (tg_id):
+############# получение суммы платежа по bill_id ################
+def getPaymentAmountByBillId (bill_id):
     # Подключиться к базе данных.
     connection = pymysql.connect(host='127.0.0.1',
                                  user='root',
@@ -31,22 +31,22 @@ def getUsersCheckByTgId (tg_id):
                                  db='paymentbot',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
-    print ("connect successful getUsersCheckByTgId!")
+    print ("connect successful getPaymentAmountByBillId!")
     try:
         with connection.cursor() as cursor:
             # SQL
-            sql = "SELECT `user_check` FROM `users` WHERE `tg_id` = %s"
+            sql = "SELECT `pay_amount` FROM `payments` WHERE `bill_id` = %s"
             # Выполнить команду запроса (Execute Query).
             # есть пользователь с этим ником в базе
-            res = cursor.execute(sql, (tg_id))
+            res = cursor.execute(sql, (id))
             results = cursor.fetchone()
             return results
     finally:
         # Закрыть соединение (Close connection).
         connection.close()
 
-############# добавление нового написавшего пользователя в базу ################
-def addNewUser (tg_id):
+############# запись нового платежа в базу ################
+async def addNewPayment (state):
     # Подключиться к базе данных.
     connection = pymysql.connect(host='127.0.0.1',
                                  user='root',
@@ -54,38 +54,17 @@ def addNewUser (tg_id):
                                  db='paymentbot',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
-    print ("connect successful addNewUser!")
-    try:
-        with connection.cursor() as cursor:
-            # SQL
-            sql = "INSERT INTO `users` (`tg_id`) VALUES (%s)"
-            res = cursor.execute(sql, (tg_id))
-            connection.commit()
-            return res
-    finally:
-        # Закрыть соединение (Close connection).
-        connection.close()
-
-############# обновление баланса юзера ################
-async def updtUsersBalance (newBalance, tg_id):
-    # Подключиться к базе данных.
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='root',
-                                 password='',
-                                 db='paymentbot',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    print ("connect successful updtUsersBalance!")
+    print ("connect successful addNewPayment!")
     cursor = connection.cursor()
     try:
         async with state.proxy() as data:
-            a = {'newBalance': newBalance, 'tg-id': tg_id}
             # SQL
-            sql = "UPDATE `users` SET "\
-            "`user_check` = %s WHERE `tg_id` = %s"
+            sql = "INSERT INTO `payments` "\
+            "(`user_tgid`, `pay_amount`, `bill_id`) "\
+            "VALUES (%s, %s, %s)"
             # Выполнить команду запроса (Execute Query).
-            # перевожу данные в кортеж для вставки
-            res = cursor.execute(sql, tuple(a.values()))
+            # есть пользователь с этим ником в базе
+            res = cursor.execute(sql, tuple(data.values()))
             connection.commit()
             return res
     finally:
--- handlers/mainhand.py
+++ handlers/mainhand.py
@@ -6,9 +6,10 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.filters import Text
 
-from db import dbusers
+from db import dbusers, dbpayments
 from keyboards import paymentAmount_kb
 from pyqiwip2p import QiwiP2P
+import random
 
 # @dp.message_handler(commands=['start', 'help'])
 async def command_start(message : types.Message):
@@ -34,7 +35,6 @@ class FSMNewpayamount(StatesGroup):
 
 @dp.callback_query_handler(text='getPaymentAmount')
 async def send_payment_amount(callback : types.CallbackQuery):
-    tg_id = callback.from_user.id
     await FSMNewpayamount.payamount.set()
     await bot.send_message(callback.from_user.id, 'Введите сумму платежа цифрами')
     await callback.answer()
@@ -42,19 +42,64 @@ async def send_payment_amount(callback : types.CallbackQuery):
 # ответ пользователя пишется в словать
 # @dp.message_handler(state=FSMNewpayamount.payamount)
 async def get_payment_amount(message: types.Message, state: FSMContext):
+    # генерация случайного id
+    randId = message.from_user.id + int(''.join([random.choice(list('123456789')) for x in range(12)]))
     paymentMount = message.text
     if paymentMount.isdigit() == True:
-        if paymentMount >= 1:
+        if int(paymentMount) >= 1:
             async with state.proxy() as data:
+                data['tg_id'] = message.from_user.id
                 data['payamount'] = message.text
+                # Выставим счет на сумму, введенную пользователем, который будет работать 5 минут
+                new_bill = p2p.bill(bill_id=randId, amount=paymentMount, lifetime=5)
+                data['new_bill'] = new_bill.bill_id
+            await dbpayments.addNewPayment(state)
             # applId = await dbusers.addNewApplicant(state)
             await state.finish()
+            forUserPayText = 'Платеж на сумму ' + str(paymentMount) + ' создан.\nНужно подтвердить его'
+            # инлайн кнопки со ссылкой для оплаты и проверкой оплаты
+            forUserPayButton = InlineKeyboardMarkup(row_width=1)
+            forUserPayButton.\
+            add(InlineKeyboardButton(text='Ссылка для оплаты', url=p2p.check(bill_id=randId).pay_url)).\
+            add(InlineKeyboardButton(text='Проверить статус платежа', callback_data='checkPay_' + new_bill.bill_id))
+            await bot.send_message(message.from_user.id, forUserPayText, reply_markup=forUserPayButton)
         else:
             await bot.send_message(message.from_user.id, 'Минимальная сумма пополнения 1 руб.')
     else:
         await bot.send_message(message.from_user.id, 'Введите целое число')
 
-
+# проверка платежа по id
+@dp.callback_query_handler(Text(startswith='checkPay_'))
+async def check_pay(callback : types.CallbackQuery):
+    # try:
+    #     bill = callback.data.split('_')[1]
+    #     if dbpayments.checkPaymentById(bill) == 1:
+    #         # Проверим статус выставленного счета через его bill_id
+    #         print(await p2p.check(bill_id=bill).status)
+    #     else:
+    #         await bot.send_message(callback.from_user.id, 'Платеж не найден')
+    # except:
+    #     await bot.send_message(callback.from_user.id, 'Возникла какая-то ошибка, попробуйте повторить команду /start')
+    #     await callback.answer()
+    bill = callback.data.split('_')[1]
+    if dbpayments.checkPaymentById(bill) == 1:
+        # Проверим статус выставленного счета через его bill_id
+        if p2p.check(bill_id=bill).status == "PAID":
+            usersBalance = dbusers.getUsersCheckByTgId(callback.from_user.id)
+            paymentAmount = dbpayments.getPaymentAmountByBillId(bill)
+            newBalance = int(usersBalance) + int(paymentAmount)
+            dbusers.updtUsersBalance(newBalance, callback.from_user.id)
+            await bot.send_message(callback.from_user.id, 'Баланс пополнен')
+        elif p2p.check(bill_id=bill).status == "WAITING":
+            # инлайн кнопки со ссылкой для оплаты и проверкой оплаты
+            forUserPayButton = InlineKeyboardMarkup(row_width=1)
+            forUserPayButton.\
+            add(InlineKeyboardButton(text='Проверить статус платежа', callback_data='checkPay_' + bill))
+            await bot.send_message(callback.from_user.id, 'Ожидает оплаты', reply_markup=forUserPayButton)
+        elif p2p.check(bill_id=bill).status == "EXPIRED":
+            await bot.send_message(callback.from_user.id, 'Платеж просрочен', reply_markup=paymentAmount_kb)
+    else:
+        await bot.send_message(callback.from_user.id, 'Платеж не найден')
 
 
 def register_handlers_client(dp : Dispatcher):
